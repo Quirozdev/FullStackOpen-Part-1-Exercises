3db0fa38fcd13ed75525b020c4e1fc2fdce4dfca
Quirozdev <luisdevv232@gmail.com>
2023-09-21 16:02:39 -0700

    uploading exercises 6-11

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import CourseApp from './course/CourseApp';
+import UnicafeApp from './unicafe/UnicafeApp';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<CourseApp />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <CourseApp />
+    <hr />
+    <UnicafeApp />
+  </>
+);
---
+++ src/unicafe/UnicafeApp.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  // check for division by 0, avoid NaN
+  const averageScore = (good - bad) / total || 0;
+
+  const positiveFeedbackPercentage = `${(good / total || 0) * 100} %`;
+  return (
+    <div>
+      <h1>Statistics</h1>
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={averageScore} />
+            <StatisticLine text="positive" value={positiveFeedbackPercentage} />
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+const UnicafeApp = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default UnicafeApp;
