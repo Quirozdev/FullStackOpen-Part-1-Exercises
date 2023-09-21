30d3038401c4d19007cda67f330ad076653c987a
Quirozdev <luisdevv232@gmail.com>
2023-09-21 16:59:34 -0700

    uploading exercises 12-14

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import CourseApp from './course/CourseApp';
 import UnicafeApp from './unicafe/UnicafeApp';
+import AnecdotesApp from './anecdotes/AnecdotesApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <CourseApp />
     <hr />
     <UnicafeApp />
+    <hr />
+    <AnecdotesApp />
   </>
 );
